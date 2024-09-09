26eed62f4e251c15b758907f027e2bdc806f708f
LuizFli <filipefln@gmail.com>
2024-09-09 17:17:57 -0300

    att

--- Aulas Progama App/atividades/src/pages/Atividade530.jsx
+++ Aulas Progama App/atividades/src/pages/Atividade530.jsx
@@ -4,20 +4,63 @@ import React, { useState } from 'react'
 
 function Atividade530() {
     const [inputNumero, setInputNumero] = useState()
-    const [resultado, setResultado] = useState()
-
+    const [resultado, setResultado] = useState() 
+    const [cor, setColor] = useState()
+    
     function ler(e){
-        console.log(e)
+        // console.log(e)
+        // setInputNumero(e.target.value)
+        switch (true) {
+            case ((e.target.value)%2 == 0 && Number(e.target.value)>0):
+                
+                setResultado('É Pár e Positivo')
+                setColor('green')
+                break;
+            case ((e.target.value)%2 == 0 ):
+                
+                setResultado('É Par')
+                setColor('brown')
+                break;
+            case (Number(e.target.value)>0):
+                
+                setResultado('É Positivo')
+                setColor('brown')
+                break;
+            case ((e.target.value)%2 != 0 && Number(e.target.value)>0):
+                
+                setResultado('Não é Par Mas é positivo')
+                setColor('brown')
+                    break;
+            case ((e.target.value)%2 == 0 && Number(e.target.value)<0):
+                
+                setResultado('É Par Mas Não é positivo')
+                setColor('brown')
+                    break;
+            case ((e.target.value)%2 != 0 && Number(e.target.value)<0):
+                
+                setResultado('Não é Par Nem é positivo')
+                setColor('brown')
+                    break;
+            default:
+                setResultado('Error')
+                setColor('brown')
+                break;
+        }
         
-
     }
 
   return (
     <div className='div-530-container'>
+        <div>
 
       <label >Digite Um Numero :</label>
-      <input type="number" placeholder='Digite Um Numero' value={inputNumero} onChange={ler}/>
+        </div>
+        <div className='div-input' style={{color: cor}}>
+
+      <input type="text" placeholder='Digite Um Numero' value={inputNumero} onChange={ler}/>
       {resultado}
+        </div>
+
     </div>
   )
 }
