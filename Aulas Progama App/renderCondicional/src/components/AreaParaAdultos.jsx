6355a0215af14738bc9b84cc92e7db0ecd44c472
LuizFli <filipefln@gmail.com>
2024-09-11 15:53:46 -0300

    att

--- Aulas Progama App/renderCondicional/src/components/AreaParaAdultos.jsx
+++ Aulas Progama App/renderCondicional/src/components/AreaParaAdultos.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AreaParaAdultos.css'
 
 function AreaParaAdultos() {
+    const [logado, setLogado] = useState(true) 
   return (
 
     <div>
         
         <p>Coisas Para Adultos</p>
-        <img className='logo' src="./public/image/InconProibido.webp"/>
+        {logado && <img className='logo' src="./public/image/InconProibido.webp"/>}
+        <button onClick={ () => {let bol = false}}>Stop Animation</button>
+        { () => {
+        let count = 0
+        const delay = 500
+        const intervalId = setInterval(() => {
+          
+          if(count%2 == 0){
+            setLogado(false)
+          }else{
+            setLogado(true)
+          }
+          count++
+          
+          if (false) {
+            
+            clearInterval(intervalId)
+          }
+        }, delay);
+        } }
+        
       
     </div>
 
